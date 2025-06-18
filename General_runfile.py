879f3291e9731eb2c86d8d0d0aac95a1ffdea6aa
GyumLee <sksmsdlrua@gmail.com>
2025-06-18 15:24:05 +0900

    first commit

---
+++ General_runfile.py
@@ -0,0 +1,472 @@
+# In[]
+
+import matplotlib.pyplot as plt
+%matplotlib qt5
+import numpy as np
+import sys
+# import os
+# sys.path.append("D:/Software/SHFQC/")
+from pathlib import Path
+
+# Helpers:
+from laboneq.contrib.example_helpers.generate_device_setup import (
+    generate_device_setup_qubits,
+)
+
+# LabOne Q:
+from laboneq.simple import *
+
+from General_functions import *
+
+from laboneq.contrib.bloch_simulator_pulse_plotter.inspector.update_inspect import (
+    pulse_update,
+)
+
+from laboneq.dsl.experiment import pulse_library
+from laboneq.contrib.bloch_simulator_pulse_plotter.inspector.update_inspect import (
+    pulse_inspector,
+)
+
+
+# In[] initial parameters for experiment (6 components available)
+
+# 같은 physical port를 공유하면, 초기 logical signal line calibration할 때
+# LO oscillator와 physical port와 관련한 변수들이 나중에 선언한 것으로 덮어씌워짐.
+# (IF oscillator만 별도로 선언 가능)
+# drive line과 drive_ef line이 서로 공유하므로, 나중에 선언한 drive_ef line의 변수들로 덮어씌워짐.
+# "q0"의 measure, acquire line과 "q1"의 measure, acquire line이 서로 공유하므로, 
+# 나중에 선언한 "q1"의 measure, acquire line의 변수들로 덮어씌워짐.
+
+physical_ports = {
+    "measure":{
+        "freq_LO": 7.2e9,   # f = f_LO + f_IF (positive sideband)
+        "port_delay": 0e-9, # Not currently supported on SHFSG output channels.
+        "port_mode": None,
+        "delay_signal": None, # Global delay for the signal line, implemented by adjusting the waveform.
+        "threshold": None,
+        "added_outputs": None, # Added outputs to the signal line's physical channel port. (RTR option enabled)
+        "automute": True, # Mute output channel when no waveform is played on it i.e for the duration of delays.
+        "range": 10,
+        "amplitude": 1.0, # it does not work!
+                          # Only supported by the SHFQA. Amplitude multiplying all waveforms played on the signal line.
+    },
+    "acquire":{
+        "freq_LO": 7.2e9,   # f = f_LO + f_IF (positive sideband) (should be same with measure line)
+        "port_delay": 76e-9, # 72e-9, (8-2) 
+        "port_mode": None,
+        "delay_signal": None, # Global delay for the signal line, implemented by adjusting the waveform.
+        "threshold": None,
+        "added_outputs": None, # Added outputs to the signal line's physical channel port. (RTR option enabled)
+        "automute": False, # Mute output channel when no waveform is played on it i.e for the duration of delays.
+        "range": -10,
+        "amplitude": 1.0, # it does not work!
+                          # Only supported by the SHFQA. Amplitude multiplying all waveforms played on the signal line.
+    },
+    "drive":{
+        "q0":{ # 1st control line port # 현재 안 쓰는 포트
+            "freq_LO": 4.2e9,
+            "port_delay": 0e-9, # Not currently supported on SHFSG output channels.
+            "port_mode": None,
+            "delay_signal": None, 
+            "threshold": None,
+            "added_outputs": None, 
+            "automute": True, 
+            "range": 10,
+            "amplitude": 1, # Only supported by the SHFQA. Amplitude multiplying all waveforms played on the signal line.
+        },
+        "q1":{ # 2nd control line port
+            "freq_LO": 6e9,
+            "port_delay": 0e-9, # Not currently supported on SHFSG output channels.
+            "port_mode": None,
+            "delay_signal": None, 
+            "threshold": None,
+            "added_outputs": None, 
+            "automute": True, 
+            "range": 10,
+            "amplitude": 1, # Only supported by the SHFQA. Amplitude multiplying all waveforms played on the signal line.
+        },
+        "q2":{ # 3rd control line port
+            "freq_LO": 4.2e9,
+            "port_delay": 0e-9, # Not currently supported on SHFSG output channels.
+            "port_mode": None,
+            "delay_signal": None, 
+            "threshold": None,
+            "added_outputs": None, 
+            "automute": True, 
+            "range": 10,
+            "amplitude": 1, # Only supported by the SHFQA. Amplitude multiplying all waveforms played on the signal line.
+        },
+        "m0":{ # mapped to port4 # 현재 안 쓰는 포트
+            "freq_LO": 5e9,
+            "port_delay": 0e-9, # Not currently supported on SHFSG output channels.
+            "port_mode": None,
+            "delay_signal": None, 
+            "threshold": None,
+            "added_outputs": None, 
+            "automute": True, 
+            "range": 10,
+            "amplitude": 1, # Only supported by the SHFQA. Amplitude multiplying all waveforms played on the signal line.
+        },
+        "m1":{ # mapped to port5
+            "freq_LO": 5e9,
+            "port_delay": 0e-9, # Not currently supported on SHFSG output channels.
+            "port_mode": None,
+            "delay_signal": None, 
+            "threshold": None,
+            "added_outputs": None, 
+            "automute": True, 
+            "range": 10,
+            "amplitude": 1, # Only supported by the SHFQA. Amplitude multiplying all waveforms played on the signal line.
+        }
+    }
+}
+
+### These parameters will be utilized in defining experimental sequences.
+qubits_parameters = {
+    # two(cr) qubit gate : each parameter is assigned to each qubit except ef_frequency;
+
+    "q0": { #control out port 1 # 현재 안 쓰는 포트
+        ### frequency parameters ###
+        "ge_frequency" : 4.3997e9-0.049e6,
+        "ef_frequency" : 4.3997e9-0.049e6,
+        "readout_frequency" : 7.4097e9, 
+        ### readout parameters ###
+        "readout_amp" : 0.17, #0.18,
+        'readout_pulse_length': 1500e-9,
+        "readout_integration_length": 1400e-9,
+        "readout_integration_amp": 1,
+        "readout_phase": (135) * np.pi/180,
+        ### drive parameters ###
+        "drive_amp" : 0.584, 
+        "drive_pulse_length": 64e-9,
+        # "drive_ef_amp" : 0.6,
+        # "drive_ef_pulse_length": 64e-9,
+        
+        "pi2_length": 64e-9,
+        "pi2_amp": 0.29,
+        "pi2_beta": 0.025,
+
+        "pi_length": 64e-9,
+        "pi_amp": 0.582,
+        "pi_beta": 0.025,
+
+        "cond_pi_length": 64e-9*6,# 64e-9*8, (8-2)
+        "cond_pi_amp": 0.071, #0.0715, (8-2)
+        "cond_pi_beta": 0.0,
+
+        "rabi_drive_amp": 0.01,
+        "ramp_length": 0e-9,
+        ### extra parameters ###
+        "reset_delay_length": 200e-6,
+
+        "is_cr" : False,
+        "cr" : {
+            "control_qubit_frequency": 4.9341e9+0.039e6,
+            "target_qubit_frequency_1": 4.7e9+0.12e6,
+            "target_qubit_frequency_2": 4.7e9,  # temporally arbitrary value
+        }
+
+    },
+    "q1": { #control out port 2 (7-1)
+        ### frequency parameters ###
+        "ge_frequency" : 6.284358e9, 
+        "ef_frequency" : 6.284358e9,
+        "readout_frequency" : 7.50824e9,
+        ### readout parameters ###
+        "readout_amp" : 0.17, #0.18,
+        'readout_pulse_length': 1500e-9,
+        "readout_integration_length": 1400e-9,
+        "readout_integration_amp": 1,
+        "readout_phase": (235) * np.pi/180,
+        ### drive parameters ###
+        "drive_amp" : 0.426, 
+        "drive_pulse_length": 64e-9,
+        # "drive_ef_amp" : 0.6,
+        # "drive_ef_pulse_length": 64e-9,
+        
+        "pi2_length": 64e-9,
+        "pi2_amp": 0.198, 
+        "pi2_beta": 0.025,
+
+        "pi_length": 64e-9,
+        "pi_amp": 0.396, 
+        "pi_beta": 0.025,
+
+        "cond_pi_length": 64e-9*6,
+        "cond_pi_amp": 0.0675, 
+        "cond_pi_beta": 0.0,
+
+        "rabi_drive_amp": 0.01,
+        "ramp_length": 0e-9,
+        ### extra parameters ###
+        "reset_delay_length": 200e-6,
+
+        "is_cr" : False,
+        "cr" : {
+            "control_qubit_frequency": 4.9341e9,
+            "target_qubit_frequency_1": 4.7e9+0.12e6,
+            "target_qubit_frequency_2": 4.7e9,
+        }
+    },
+
+    "q2": { #control out port 1 (8-2)
+        ### frequency parameters ###
+        "ge_frequency" : 4.3997e9-0.049e6-0.134e6,
+        "ef_frequency" : 4.3997e9-0.049e6,
+        "readout_frequency" : 7.4097e9, 
+        ### readout parameters ###
+        "readout_amp" : 0.17, #0.18,
+        'readout_pulse_length': 1500e-9,
+        "readout_integration_length": 1400e-9,
+        "readout_integration_amp": 1,
+        "readout_phase": (195) * np.pi/180,
+        ### drive parameters ###
+        "drive_amp" : 0.584, 
+        "drive_pulse_length": 64e-9,
+        # "drive_ef_amp" : 0.6,
+        # "drive_ef_pulse_length": 64e-9,
+        
+        "pi2_length": 64e-9,
+        "pi2_amp": 0.299,
+        "pi2_beta": 0.029,
+
+        "pi_length": 64e-9,
+        "pi_amp": 0.6,
+        "pi_beta": 0.029,
+
+        "cond_pi_length": 768e-9,
+        "cond_pi_amp": 0.049,
+        "cond_pi_beta": 0.0,
+
+        "rabi_drive_amp": 0.01,
+        "ramp_length": 0e-9,
+        ### extra parameters ###
+        "reset_delay_length": 200e-6,
+
+        "is_cr" : False,
+        "cr" : {
+            "control_qubit_frequency": 4.9341e9+0.039e6,
+            "target_qubit_frequency_1": 4.7e9+0.12e6,
+            "target_qubit_frequency_2": 4.7e9,  # temporally arbitrary value
+        }
+
+    },
+}
+
+cavity_parameters = {
+    'm0': { # port 4 # 현재 안 쓰는 포트
+        "mode_frequency": 5.128112e9, # omega_d
+        "cavity_drive_length": 120e-9,
+        "cavity_drive_amp": 0.1, #0.02,
+        "reset_delay_length": 700e-6,
+
+        "cond_disp_pulse_length": 200e-9, #200e-9,
+        "cond_disp_pulse_amp": 0.9, #0.5,
+        "alpha_1_CNOD_amp": 0.1104, # 1 when it is not found yet.
+
+        "cavity_mode_chi": -0.81e6, # driving cavity mode of ground state in ground state frame.
+        "cond_disp_pulse_frequency": 0.05e6, #0.08e6,
+        "cond_disp_pulse_detuning": 0.05e6,
+        "cond_disp_pulse_sigma": 2,
+    },
+    'm1': { # port 5
+        # 5.128217e9 : ground state freq
+        "mode_frequency": 5.128217e9, # omega_d
+        "cavity_drive_length": 40e-9,
+        "cavity_drive_amp": 0.066,#0.03, (this is the maximum amplitude range, and this goes to amp of experiment)
+        "alpha_1_cavity_drive_amp": 0.033, # 1 when it is not found yet. (This is amp when photon = 1) (this should be same with scaling_factor in disp_amp_calibration_parity
+        "reset_delay_length": 640e-6,
+
+        "cond_disp_pulse_length": 200e-9, #200e-9,
+        "cond_disp_pulse_amp": 1j*0.1147/2, #1j*0.1147/3, #0.5, (this is the maximum amplitude range, and this goes to amp of experiment)
+        "alpha_1_CNOD_amp": 0.1147, # 1 when it is not found yet. (this should be same with scaling_factor in CNOD calibration)
+
+        # (omega_d : w_LO + w_IF , omega_e or g : w_LO + w_IF + cond_disp_pulse_frequency)
+        "cavity_mode_chi": -0.947e6, #-0.824e6,
+        "cond_disp_pulse_frequency": -0.824e6, # driving only cavity mode of ground state in driving freq(mode_frequency) frame.
+        "cond_disp_pulse_detuning": 0.05e6, 
+        "cond_disp_pulse_sigma": 2, #4,
+    }
+
+}
+
+# In[]
+my_run = ZI_QCCS(physical_ports, qubits_parameters, cavity_parameters, 
+                 number_of_qubits=3,
+                 number_of_memory_modes=2,
+                 is_memory_mode = True, 
+                 which_qubit=2,
+                 which_mode =1,
+                 which_data= "I", 
+                 cr_drive_lines=False, 
+                 multiplex_drive_lines = False, 
+                 use_emulation= False)
+
+# %% propagation delay calibration for readout and drive line
+
+my_run.prop_delay_calibration(line = "readout", average_exponent=12)
+
+
+# %%
+my_run.nopi_pi(average_exponent=12, phase = 195, is_plot_simulation=False) # used for 2^n averages, n=average_exponent, maximum: n = 19
+my_run.single_shot_nopi_pi(npts_exponent = 12, phase = 195)
+my_run.plot_nopi_pi(npts = 100)
+
+#In[]
+
+my_run.T1(average_exponent=11, duration = 30e-6, npts = 61, is_plot_simulation=False)
+my_run.plot_T1()
+
+# In[]
+
+my_run.Pi2_cal(average_exponent=12, start = 0, npts = 12, is_plot_simulation=False)
+my_run.plot_Pi2_cal()
+
+# In[]
+
+my_run.Pi_cal(average_exponent=12, npts = 18, is_plot_simulation=False, is_cond_pulse=False)
+my_run.plot_Pi_cal()
+
+# In[]
+my_run.Ramsey(is_echo = False, detuning = 0.5e6, average_exponent=11, duration = 5e-6, npts = 41,
+              is_zz_interaction= False,
+              control_qubit= 1,
+              is_plot_simulation= False)
+my_run.plot_Ramsey()
+
+
+# %%
+my_run.error_amplification(average_exponent= 10, pulse_npts = 18, amp_npts = 41, 
+                           start_amp = -0.05, end_amp = 0.1,
+                           is_drag_beta_calibration = True, #if True, beta will be swept
+                           is_plot_simulation=False)
+my_run.plot_error_amplification(is_drag_beta_calibration = True)
+ # In[]
+# my_run.Rabi_amplitude(average_exponent=12, npts = 101, is_plot_simulation=True)
+
+# In[]
+my_run.Rabi_length(average_exponent=11, duration = 20e-6, npts = 101, is_plot_simulation=False)
+my_run.plot_Rabi_length()
+
+# In[] cr_calibration_amp
+my_run.cr_calibration_amp(average_exponent=12, npts = 101, control_qubit = 0, target_qubit = 1, is_plot_simulation=False)
+my_run.plot_cr_calibration_amp()
+
+# In[] cr_calibration_length
+
+my_run.cr_calibration_length(average_exponent=12, npts = 101, control_qubit = 0, target_qubit = 1,
+                             duration = 1e-6, target_qubit_amp = 0.5,
+                             is_plot_simulation=False)
+my_run.plot_cr_calibration_length()
+
+
+# In[]
+my_run.All_XY(average_exponent=12, is_plot_simulation=False)
+my_run.plot_All_XY()
+
+
+# In[] drag_calibration
+my_run.drag_calibration(average_exponent=15, beta_start = -0.2, beta_stop = 0.2, beta_count = 81, is_plot_simulation=False)
+my_run.plot_drag_calibration()
+
+# In[] cavity_T1
+
+my_run.cavity_T1(average_exponent=10, duration = 300e-6, npts = 51, is_plot_simulation=False)
+my_run.plot_cavity_T1()
+
+# In[] cavity_mode_spectroscopy
+
+my_run.cavity_mode_spectroscopy(average_exponent=12, freq_start = 110e6,
+                                freq_stop = 150e6, npts = 121, is_plot_simulation=False)
+
+my_run.plot_cavity_mode_spectroscopy()
+
+# In[]
+
+my_run.cavity_pi_nopi(average_exponent=9, freq_start = -0.25e6, freq_stop = 1.25e6, 
+                      freq_npts = 81, 
+                      is_qubit2 = False, 
+                      qubit2 = 1,
+                      is_plot_simulation=False)
+# not accurate for finding chi with cavity_pi_nopi experiment
+
+my_run.plot_cavity_pi_nopi()
+
+
+# In[]
+
+my_run.CNOD_calibration(average_exponent=10, amp_range=0.5, npts=101, qubit_phase = 0, is_displaced_state= True, is_plot_simulation=False)
+my_run.plot_CNOD_calibration(scaling_factor=0.1147)
+
+# In[]
+
+my_run.Char_func_displaced(average_exponent=10, npts=41, qubit_phase =0, is_plot_simulation=False)
+my_run.plot_Char_func_displaced()
+
+# In[]
+
+my_run.disp_pulse_calibration_geophase(average_exponent=11, amp_sweep = 1, amp_npts=51, is_plot_simulation=False)
+my_run.plot_disp_pulse_calibration_geophase()
+
+# In[]
+
+my_run.disp_pulse_calibration_parity(average_exponent=10, amp_start = -1, amp_stop=1, amp_npts = 81, is_plot_simulation=False)
+my_run.plot_disp_pulse_calibration_parity(is_fit=True, scaling_factor=0.033) # scaling factor -> alpha_1_cavity_drive_amp 
+                                                                        # sigma should be 0.5 by adjusting scaling factor
+
+# %%
+
+my_run.out_and_back_measurement(average_exponent=9, init_state = "g",
+                                 phase_start= -30, phase_stop=30, # deg
+                                 cavity_drive_amp_start = 0, cavity_drive_amp_stop = 1, # final amp will be multiplying it by "cavity_drive_amp"
+                                 phase_npts=21, amp_npts=41,
+                                 wait_time=1e-6,
+                                 is_plot_simulation=False)
+
+my_run.plot_out_and_back_measurement(Disp_amp_exp=0.033, Photon_number_exp= 1, 
+                                    fitting=True, x_threshold=10, wait_time=1e-6, init_state="g")
+
+# In[] finding chi between cavity mode and qubit
+
+my_run.qubit_state_revival(average_exponent=11, wait_time=2.2e-6, wait_npts = 101, is_plot_simulation=False)
+my_run.plot_qubit_state_revival()
+
+# In[]
+
+my_run.storage_mode_characterization(average_exponent=10, 
+                                    wait_time = 20e-6,
+                                    wait_npts = 101,
+                                    detuning = 0.1e6,
+                                    is_plot_simulation=False)
+my_run.plot_storage_mode_characterization()
+
+
+
+
+
+# In[]
+## define pulse
+my_pulse = pulse_library.cond_disp_pulse(uid="my_pulse", length=100e-9, 
+                                         amplitude=1.0, sigma = 1,
+                                         chi = 1e6, detuning = 0.001e6,
+                                         zero_boundaries=False)
+
+cavity_drive_pulse = pulse_library.gaussian(
+                uid="cavity_drive_pulse",
+                length=cavity_parameters["m1"]["cavity_drive_length"],
+                amplitude=cavity_parameters["m1"]["cavity_drive_amp"]
+            )
+
+pulse = pulse_update(
+            cavity_drive_pulse,
+            spectral_window = None,
+            flip_angle = None,
+            pulse_parameters=cavity_drive_pulse.pulse_parameters,
+        )
+
+my_run.analyze_pulse(cavity_drive_pulse)
+
+
+
+
+# %%
---
+++ smart_fit.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  8 13:44:12 2024
+
+@author: user
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.fft import fft, ifft, fftshift, ifftshift
+from numpy import pi, cos, sin, exp, real, imag
+from scipy.optimize import curve_fit, minimize
+import msvcrt
+from os.path import isfile
+from time import time, sleep
+import scipy.stats
+from scipy.interpolate import interp1d
+
+# In[]
+
+class sFit():
+    
+    def __init__(self, fit_type, xdata, ydata):
+        
+        self.fit_type = fit_type
+        self.xdata = xdata
+        self.ydata = ydata
+        self.t0 = xdata[0]
+        self.delta_t = np.diff(xdata)[0]
+        
+        if (self.fit_type != 'Cos') and (self.fit_type != 'Exp') and (self.fit_type != 'ExpCos') and (self.fit_type != 'Gaussian') and (self.fit_type != 'GaussianCos'):
+            raise Exception("No such a type!")
+        
+        func = self.fit_function()
+        self.func = func
+        self.initial_guess = self.smart_guess()
+        
+        
+        # return curve_fit(func, self.xdata, self.ydata)
+    
+    def fit_function(self):
+        
+        if self.fit_type == 'Cos':
+            return self.Cos
+        elif self.fit_type == 'Exp':
+            return self.Exp
+        elif self.fit_type == 'ExpCos':
+            return self.ExpCos
+        elif self.fit_type == 'Gaussian':
+            return self.Gaussian
+        elif self.fit_type == 'GaussianCos':
+            return self.GaussianCos
+        
+    def _curve_fit(self):
+        return curve_fit(self.func, self.xdata, self.ydata, p0 = self.initial_guess, maxfev=10000)
+    
+    def smart_guess(self):
+        
+        
+        if self.fit_type == 'Cos':
+            
+            Amp = (max(self.ydata) - min(self.ydata))/2
+            Offset = np.mean(self.ydata)
+            
+            [freq, delta] = self._fft_results()
+            
+            print("initial_guess : amp={0}, freq={1}, delta={2}, offset={3}".format(Amp, freq, delta, Offset))
+            
+            return [Amp, freq, delta, Offset]
+        
+        elif self.fit_type == 'Exp':
+            
+            Amp = max(self.ydata) - min(self.ydata)
+            Gamma = 1e5
+            Offset = (max(self.ydata) - min(self.ydata))/2
+            
+            return [Amp, Gamma, Offset]
+        
+        elif self.fit_type == 'ExpCos':
+            
+            Amp = max(self.ydata) - min(self.ydata)
+            [freq, delta] = self._fft_results()
+            Gamma = 0
+            Offset = (max(self.ydata) - min(self.ydata))/2
+            
+            return [np.abs(Amp), freq, Gamma, delta, Offset]
+        
+        elif self.fit_type == 'Gaussian':
+
+            Amp = max(self.ydata) - min(self.ydata) if self.ydata[len(self.ydata)//2] > self.ydata[0] else min(self.ydata) - max(self.ydata)
+            sigma = self.estimate_sigma_from_fwhm(self.xdata, self.ydata)
+            Offset = self.ydata[-1]
+            t0 = 0
+
+            print("initial_guess : amp={0}, sigma={1}, offset={2}, t0={3}".format(Amp, sigma, Offset, t0))
+            
+            return [Amp, sigma, Offset, t0]
+        
+        elif self.fit_type == 'GaussianCos':
+
+            Amp = (max(self.ydata) - min(self.ydata))/2
+            [freq, delta] = self._fft_results()
+            sigma = 1
+            Offset = np.mean(self.ydata)
+            t0 = np.mean(self.xdata)
+
+            print("initial_guess : amp={0}, freq={1}, delta={2}, offset={3}, t0={4}".format(Amp, freq, delta, Offset, t0))
+            
+            
+            return [Amp, freq, sigma, delta, Offset, t0]
+
+
+    
+    def _fft_results(self): 
+        
+        """return FT results, (freq, phase)"""
+        
+        fft_y = fftshift(fft(self.ydata))
+        freqs = self.freq_axis( self.delta_t, length = len(self.ydata))
+        
+        freq = self.peak_freq(fft_y, freqs)
+        delta = np.angle(fft_y[freqs == freq])[0]
+        
+        return [freq, delta]
+            
+    def freq_axis(self, delta_t, length):
+        """returns an array of frequencies"""
+        
+        return np.linspace(-1/(2*delta_t), 1/(2*delta_t), length)
+        # return np.linspace(-1/(2*dt), 1/(2*dt) - 1/(length*dt), length)
+    
+    def peak_freq(self, fft_y, freqs):
+        
+        """Find the peak frequency in an fft trace. Assumes fftshift has been applied, and ignores the DC componnent
+        common usage: peak_freq(*fft_trace(trace, dt)) """
+        
+        n = len(fft_y)
+        startInd = int(n/2+1)
+        if n > 4:
+            
+            amps = abs(fft_y[startInd:n-1])
+            maxval = max(amps)
+            max_index = np.where(amps==maxval)[0][0]
+            
+            return freqs[startInd + max_index]
+        else:
+            return freqs[0]*1e9 # if fft_y is too short, just return some frequency
+    
+    def estimate_sigma_from_fwhm(self, x_data, y_data):
+        y_data = np.array(y_data)
+        x_data = np.array(x_data)
+
+        # Step 1: Find half
+        half = (np.max(y_data) + np.min(y_data))/2.0
+
+        # Step 2: Find indices where curve crosses half max
+        above = y_data > half
+        crossing_indices = np.where(np.diff(above.astype(int)) != 0)[0]
+
+        if len(crossing_indices) < 2:
+            raise ValueError("Cannot determine FWHM: need two crossing points.")
+
+        # Step 3: Interpolate to find exact crossing points
+        x_crossings = []
+        for idx in crossing_indices:
+            f = interp1d(y_data[idx:idx+2], x_data[idx:idx+2])
+            x_crossings.append(float(f(half)))
+
+        # Step 4: Compute FWHM and sigma
+        fwhm = abs(x_crossings[1] - x_crossings[0])
+        sigma = fwhm / 2.3548
+
+        return sigma
+   
+    
+    # [traceFFT, traceFreqs] = self.fft_trace( trace, dt)
+    # Freq = self.peak_freq(traceFFT, traceFreqs)
+    # Amp = max(trace)-min(trace)
+    # if np.where(trace == max(trace))[0][0] > np.where(trace == min(trace))[0][0]: Amp = -Amp
+    # Offset = (max(trace)-min(trace))/2
+    # if trace.mean() < 0: Offset = -Offset 
+    # Gamma = 0
+    # Delta = np.angle(traceFFT[traceFreqs == Freq])[0]
+    # return Amp, Freq, Gamma, Delta, Offset
+        
+    def Line(self, x, a, b):
+        return (a*x + b).astype(np.float64)
+    def Poly2(x, a, b, c):
+        return (a*x**2 + b*x + c).astype(np.float64)
+    def Exp(self, t, A, gamma, offset): # 3 args
+        return (A*exp(-t*gamma) + offset - A/2).astype(np.float64)
+    def Cos(self, t, A, f, delta, offset): # 4 args
+        return (A*cos((t-self.t0)*2*pi*f+delta)+offset).astype(np.float64)
+    def ExpCos(self,  t, A, f, gamma, delta, offset): # 5 args
+        return (A*exp(-t*gamma)*cos((t-self.t0)*2*pi*f+delta) + offset).astype(np.float64)
+    def ExpExp(self,  t, A, alphaSize , kappa, offset ):
+        return (A*(1-exp( - abs( alphaSize ) * exp( -kappa*t ) )) + offset).astype(np.float64)
+    def Gaussian(self, t, A, sigma, offset, t0): # 3 args
+        return (A * exp(-((t-t0)**2)/(2*sigma**2)) + offset).astype(np.float64)
+    def GaussianCos(self, t, A, f, sigma, delta, offset, t0): # 5 args
+        return (A * exp(-((t-t0)**2)/(2*sigma**2)) * cos((t-t0)*2*pi*f + delta) + offset).astype(np.float64)
